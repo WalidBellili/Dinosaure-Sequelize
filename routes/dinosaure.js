059e5d8794247ae7d5d4099ed359eed35bcf0920
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-05 16:07:11 +0200

    put and delete ok

--- routes/dinosaure.js
+++ routes/dinosaure.js
@@ -23,4 +23,57 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dinosaure = await Dinosaure.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (dinosaure) {
+      res.json(dinosaure);
+    } else {
+      res.status(404).json("Dinosaure not found");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json("Internal server error");
+  }
+});
+
+app.put("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dinosaure = await Dinosaure.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    res.json(dinosaure);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json("Internal server error");
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Dinosaure.destroy({
+      where: { id },
+    });
+
+    res.status(204);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json("Internal server error");
+  }
+});
+
 module.exports = app;
